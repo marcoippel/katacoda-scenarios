0070c660a21db70518d7ca8489bbfbf99a40af35
Marco Ippel <marco@ippelsoft.nl>
2020-11-21 21:35:23 +0100

    Add background task

--- K8s-Basics/addHelmRepo.sh
+++ K8s-Basics/addHelmRepo.sh
@@ -3,5 +3,5 @@ echo "wait till kubernetes is ready"
 sleep 15
 helm repo add k8sstudy https://k8sstudy.blob.core.windows.net/helm/
 helm install k8sstudy k8sstudy/k8sstudy
-alias k="kubeclt"
+alias k="kubectl"
 k config user-context --current --namespace=study
