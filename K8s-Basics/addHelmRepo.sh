3773e4fde93e201b2ae95d7b0c749026e04523bd
Marco Ippel <marco@ippelsoft.nl>
2020-11-21 22:39:12 +0100

    Script updated

--- K8s-Basics/addHelmRepo.sh
+++ K8s-Basics/addHelmRepo.sh
@@ -3,5 +3,5 @@ echo "Wait till kubernetes is ready"
 sleep 30
 #helm repo add k8sstudy https://k8sstudy.blob.core.windows.net/helm/
 #helm install k8sstudy k8sstudy/k8sstudy
-alias k="kubectl"
-k config set-context --current --namespace=study
+#alias k="kubectl"
+#kubectl config set-context --current --namespace=study
